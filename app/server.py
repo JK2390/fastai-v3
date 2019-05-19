8ffdcdfa9a975821cd8350847dcbce8dc0557819
Julian Kleinz <40247455+JK2390@users.noreply.github.com>
2019-05-19 12:07:18 +0200

    customize the app for personal model
    model_file_url & classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/2nsmaicfh7d1x49/Sports-Ball-Games.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['american_football', 'baseball', 'basketball', 'cricket', 'golf', 'handball', 'lacrosse', 'rugby', 'soccer', 'squash', 'tennis', 'volleyball']
 path = Path(__file__).parent
 
 app = Starlette()
